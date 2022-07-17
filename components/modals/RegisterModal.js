7d0c4bb49c15c2aca10ed71f15e6384c0c0cb70b
mendingbook <jesse.robinson004@gmail.com>
2022-07-17 12:42:07 -0500

    Added register username and password requirements

--- components/modals/RegisterModal.js
+++ components/modals/RegisterModal.js
@@ -1,18 +1,46 @@
+import { useRef, useState } from "react";
 import axios from "axios";
 
 export default function RegisterModal(props) {
   const usernameRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
+  const [passwordSame, setPasswordSame] = useState(true);
+  const [passwordLength, setPasswordLength] = useState(true);
+  const [usernameLength, setUsernameLength] = useState(true);
+  const [userAlreadyUsed, setUserAlreadyUsed] = useState(false);
+
+  // Removed password requirements
+  //const [passwordUppercase, setPasswordUppercase] = useState(false);
+  //const [passwordLowercase, setPasswordLowercase] = useState(false);
+  //const [passwordNumber, setPasswordNumber] = useState(false);
+  //const [passwordSpecialChar, setPasswordSpecialChar] = useState(false);
+  //const [passwordWhitespace, setPasswordWhitespace] = useState(false);
+
   function registerHandler(event) {
     event.preventDefault();
     const username = usernameRef.current.value;
     const password = passwordRef.current.value;
     const confPassword = confirmPasswordRef.current.value;
+    if (password != confPassword) {
+      setPasswordSame(false);
+      return;
+    }
+    if (password.length < 3 || password.length > 16) {
+      setPasswordLength(false);
+      return;
+    }
+    if (username.length < 3 || username.length > 16) {
+      setUsernameLength(false);
+      return;
+    }
     axios
       .post(`${process.env.NEXT_PUBLIC_APIURL}/user/register`, {
         username: username,
         pass: password,
+      })
+      .catch((error) => {
+        setUserAlreadyUsed(true);
       })
       .then((response) => {
         if (response !== undefined) {
@@ -22,6 +50,35 @@ export default function RegisterModal(props) {
         }
       });
   }
+
+  function onUsernameChange() {
+    setUserAlreadyUsed(false);
+    if (
+      usernameRef.current.value.length < 3 ||
+      usernameRef.current.value.length > 16
+    ) {
+      setUsernameLength(false);
+    } else {
+      setUsernameLength(true);
+    }
+  }
+
+  function onPasswordChange() {
+    if (
+      passwordRef.current.value.length < 3 ||
+      passwordRef.current.value.length > 16
+    ) {
+      setPasswordLength(false);
+    } else {
+      setPasswordLength(true);
+    }
+    if (passwordRef.current.value != confirmPasswordRef.current.value) {
+      setPasswordSame(false);
+    } else {
+      setPasswordSame(true);
+    }
+  }
+
   return (
     <div className="fixed z-50 inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="w-[28rem] h-auto flex flex-col p-4 bg-neutral-800 rounded-xl gap-1">
@@ -52,31 +109,64 @@ export default function RegisterModal(props) {
           <div className="flex flex-col">
             <span className="text-neutral-50 font-semibold">Username</span>
             <input
+              onChange={onUsernameChange}
               type="text"
               ref={usernameRef}
               className="text-white bg-neutral-900 py-3 px-2 rounded-md border-neutral-700 border-[1px] hover:border-blue-600 duration-300 outline-0 focus:border-blue-600"
               placeholder="Enter your username..."
             />
+            {!usernameLength && (
+              <div className="flex flex-row gap-1 items-center">
+                <img src="/svgs/Red Alert.svg" className="w-6 h-6" />
+                <span className="text-red-500">
+                  Username must be between 3 and 16 characters!
+                </span>
+              </div>
+            )}
+            {userAlreadyUsed && (
+              <div className="flex flex-row gap-1 items-center">
+                <img src="/svgs/Red Alert.svg" className="w-6 h-6" />
+                <span className="text-red-500">Username already in use!</span>
+              </div>
+            )}
           </div>
           <div className="flex flex-col">
             <span className="text-neutral-50 font-semibold">Password</span>
             <input
+              onChange={onPasswordChange}
               type="password"
               ref={passwordRef}
               className="text-white bg-neutral-900 py-3 px-2 rounded-md border-neutral-700 border-[1px] hover:border-blue-600 duration-300 outline-0 focus:border-blue-600"
               placeholder="Enter your password..."
             />
+            {!passwordLength && (
+              <div className="flex flex-row gap-1 items-center">
+                <img src="/svgs/Red Alert.svg" className="w-6 h-6" />
+                <span className="text-red-500">
+                  Password must be between 3 and 16 characters!
+                </span>
+              </div>
+            )}
           </div>
           <div className="flex flex-col">
             <span className="text-neutral-50 font-semibold">
               Confirm Password
             </span>
             <input
+              onChange={onPasswordChange}
               type="password"
               ref={confirmPasswordRef}
               className="text-white bg-neutral-900 py-3 px-2 rounded-md border-neutral-700 border-[1px] hover:border-blue-600 duration-300 outline-0 focus:border-blue-600"
               placeholder="Enter your password..."
             />
+            {!passwordSame && (
+              <div className="flex flex-row gap-1 items-center">
+                <img src="/svgs/Red Alert.svg" className="w-6 h-6" />
+                <span className="text-red-500">
+                  Passwords must be the same!
+                </span>
+              </div>
+            )}
           </div>
           <button
             type="submit"
